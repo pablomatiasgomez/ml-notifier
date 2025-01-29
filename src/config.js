8415f036aaaed39f1f41d2eed3b32de759749021
Pablo Matias Gomez <pablomatiasgomez@gmail.com>
2025-01-29 00:00:00 -0300

    get rid of telegram dep

--- src/config.js
+++ src/config.js
@@ -1,11 +1,11 @@
 'use strict';
 
 let Config = {
-    "telegram": {
-        "token": null,
-        // Can be retrieved using https://api.telegram.org/bot{TOKEN}/getUpdates
-        "chatId": null
-    }
+	"telegram": {
+		"token": null,
+		// Can be retrieved using https://api.telegram.org/bot{TOKEN}/getUpdates
+		"chatId": null
+	}
 };
 
 module.exports = Config;
--- src/notifier-service.js
+++ src/notifier-service.js
@@ -1,55 +1,77 @@
 'use strict';
 
-// TODO: migrate to "node-telegram-bot-api" once that dependency stops using "request"..
-const TelegramBot = require('telegram-bot-api');
-
 const Utils = require('./utils.js');
 
 //----------------------
 
 const TELEGRAM_MESSAGE_BYTES_LIMIT = 4096;
+const TELEGRAM_BASE_URL = 'https://api.telegram.org'
 
 class NotifierService {
-    constructor() {
-        this.telegramBot = new TelegramBot({
-            token: config.telegram.token
-        });
-        this.chatId = config.telegram.chatId;
-    }
+	constructor() {
+		this.token = config.telegram.token;
+		this.chatId = config.telegram.chatId;
+	}
 
-    notify(message) {
-        let promise = Promise.resolve();
-        this.splitMessage(message).forEach(message => {
-            promise = promise.then(() => {
-                return this.telegramBot.sendMessage({
-                    chat_id: this.chatId,
-                    text: message,
-                    parse_mode: 'HTML',
-                }).catch(e => {
-                    console.error("Error while notifying to telegram.. ", e);
-                });
-            }).then(Utils.delay(200));
-        });
-        return promise;
-    }
+	/**
+	 * notify will notify to the configured chatId, by splitting the message if needed.
+	 * @param {string} message
+	 * @returns {Promise<void>}
+	 */
+	notify(message) {
+		let promise = Promise.resolve();
+		this.splitMessage(message).forEach(message => {
+			promise = promise.then(() => {
+				return this.sendHTMLMessage(message).catch(e => {
+					console.error("Error while notifying to telegram.. ", e);
+				});
+			}).then(Utils.delay(200));
+		});
+		return promise;
+	}
 
-    // Splits the message into multiple messages so that it fits telegram's max byte limit.
-    splitMessage(message) {
-        if (message.length <= TELEGRAM_MESSAGE_BYTES_LIMIT) {
-            return [message];
-        }
+	/**
+	 * Splits a message into multiple messages if it exceeds Telegram's character limit.
+	 *
+	 * @param {string} message - The message to be split.
+	 * @returns {string[]} - An array of message segments.
+	 */
+	splitMessage(message) {
+		if (message.length <= TELEGRAM_MESSAGE_BYTES_LIMIT) {
+			return [message];
+		}
 
-        let splitIndex = message.lastIndexOf('\n', TELEGRAM_MESSAGE_BYTES_LIMIT);
-        console.log(`Splitting message of length ${message.length} at index ${splitIndex}`);
+		let splitIndex = message.lastIndexOf('\n', TELEGRAM_MESSAGE_BYTES_LIMIT);
+		console.log(`Splitting message of length ${message.length} at index ${splitIndex}`);
 
-        let firstMessage = message.substring(0, splitIndex); // Not including the \n as it will be split into two messages
-        let secondMessage = message.substring(splitIndex + 1);
+		let firstMessage = message.substring(0, splitIndex); // Not including the \n as it will be split into two messages
+		let secondMessage = message.substring(splitIndex + 1);
 
-        return [
-            firstMessage,
-            ...this.splitMessage(secondMessage),
-        ];
-    }
+		return [firstMessage, ...this.splitMessage(secondMessage)];
+	}
+
+	/**
+	 * Actually sends the message to TelegramAPI.
+	 * @param message
+	 * @returns {Promise<void>}
+	 */
+	sendHTMLMessage(message) {
+		return fetch(TELEGRAM_BASE_URL + '/bot' + this.token + '/sendMessage', {
+			method: 'POST',
+			body: new URLSearchParams({
+				chat_id: this.chatId,
+				text: message,
+				parse_mode: 'HTML',
+			}),
+		}).then(response => {
+			if (response.status !== 200) throw new Error(`Error while executing sendMessage to TelegramAPI: ${response.status}`);
+			return response.json();
+		}).then(response => {
+			if (!response.ok) throw new Error(`Error while executing sendMessage to TelegramAPI: ${response}`);
+			return response;
+		});
+	}
+
 }
 
 module.exports = NotifierService;
